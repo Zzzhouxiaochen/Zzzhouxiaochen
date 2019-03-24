24b801c9979c54f4026a697c8b39359382f34bf0
Zzzhouxiaochen <zxc2890326540@163.com>
2019-03-24 21:12:25 +0800

    3.24

---
+++ 数据结构链表的个人练习.cpp
@@ -0,0 +1,25 @@
+//单链表：
+
+//单链表的定义;
+typedef struct LinkList
+{
+	int v;  //数据部分
+	struct LinkList *next;  //指针部分
+}LinkList;
+
+//链表初始化：
+LinkList* L = NULL; //链表的第一个地址就是初始化
+
+//链表的基本操作：
+//增，删，查，改
+
+
+//增
+//头插
+void toucha()
+{
+	LinkList *L = NULL; 
+	L->
+}
+//尾插
+//中间插
